e808982d67f81ff5247ebfae784fcfc33711c43d
Sharath <sharath@MAC-LEAPLAP003.local>
2024-01-30 13:22:01 +0530

    changed output

--- src/secondGitBasics.java
+++ src/secondGitBasics.java
@@ -2,5 +2,5 @@ public class secondGitBasics {
     public static void main(String[] args)
 
     {
-        System.out.println("Welcome second Git");
+        System.out.println("Welcome third Git");
     }}
