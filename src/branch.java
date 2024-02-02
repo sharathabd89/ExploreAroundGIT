5da817d8df0336371da1b457c647a5e859f10fc4
sharathabd89 <sharathabd89@gmail.com>
2024-02-02 14:16:57 +0530

    learnt static

--- src/branch.java
+++ src/branch.java
@@ -1,6 +1,34 @@
 public class branch {
-    public static void main(String args[])
+    private static String university_name;
+    private static String college_name;
+
+    private String student_name;
+    private String student_roll;
+
+    private String student_dept;
+
+    public void Studentobject(String student_name,String student_dept,String student_roll)
     {
-        System.out.println("hsdjjkdjjdgi");
+        this.student_name=student_name;
+        this.student_dept=student_dept;
+        this.student_roll=student_roll;
+
+    }
+    static
+    {
+        university_name="SRM University";
+        college_name="SDM_IT";
+    }
+    public static void main(String args[])
+
+    {
+        branch b=new branch();
+        b.Studentobject("sharath","CS","201819");
+        System.out.println("Student univeristy="+university_name+" "+"Student college="+college_name+" "+"Student name="+
+                b.student_name+" "+"student dept="+b.student_dept+" "+"student roll="+ b.student_roll);
+
+        b.Studentobject("prakash","CIVIL","637733");
+        System.out.println("Student univeristy="+university_name+" "+"Student college="+college_name+" "+"Student name="+
+                b.student_name+" "+"student dept="+b.student_dept+" "+"student roll="+ b.student_roll);
     }
 }
